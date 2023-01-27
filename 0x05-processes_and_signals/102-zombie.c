63650f839ae805aa76ab9480de494ace01e0554d
phemsie <femi_oworu@yahoo.com>
2023-01-27 23:18:27 +0100

    processes_and_signals

---
+++ 0x05-processes_and_signals/102-zombie.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+/**
+ * infinite_while - infinite loop parent process
+ * Return: 0 Success
+ */
+
+int infinite_while(void)
+{
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
+}
+
+/**
+ * main - program creates 5 zombie processes
+ * Return: 0 Success
+ */
+
+int main(void)
+{
+	int i;
+	i = 0;
+	for (i = 0; i < 5; i++)
+	{
+		if (fork() == 0)
+		{
+			printf("Zombie process 
+					reated, PID: %d\n", getpid());
+			exit(0);
+		}
+	}
+	infinite_while();
+	return (0);
+}
