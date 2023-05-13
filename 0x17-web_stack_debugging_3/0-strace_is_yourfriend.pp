abf66281b056c70a6bebb8283edb968f43e30cd4
phemsie <femi_oworu@yahoo.com>
2023-05-13 21:22:36 +0100

    web_stack_debugging_3

--- 0x17-web_stack_debugging_3/0-strace_is_yourfriend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_yourfriend.pp
@@ -1,23 +1,5 @@
-class apache_error_fix {
-
-  # Define a custom fact to get the Apache process ID
-  # Note: This fact will only work on Ubuntu systems
-  $apache_pid = $facts['service_apache2_pid']
-
-  # Define the strace command to trace the Apache process system calls
-  exec { 'strace_apache':
-    command => "/usr/bin/strace -p ${apache_pid} -s 10000 -o /tmp/strace.log",
-    require => Service['apache2'],
-    subscribe => Service['apache2'],
-    refreshonly => true,
-    notify => Exec['fix_apache_error'],
-  }
-
-  # Define the command to fix the Apache error found using strace
-  exec { 'fix_apache_error':
-    command => "/bin/sed -i 's/old_string/new_string/g' /path/to/apache/config/file.conf",
-    refreshonly => true,
-    notify => Service['apache2'],
-  }
-  
+# correct the file name
+exec { 'fix wp-settings.php':
+  command  => "sed -i 's/class-wp-locale.phpp/class-wp-locale.php/g' /var/www/html/wp-settings.php",
+  provider => shell,
 }
